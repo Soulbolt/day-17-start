58a0f6c25294341213d1145af3ed2ce1964da531
Soulbolt <soulbolt@gmail.com>
2023-12-12 22:16:00 -0700

    Added code and comments covering the creation of a method and default attributes

--- main.py
+++ main.py
@@ -4,6 +4,15 @@ class User:
     def __init__(self, user_id, username):
         # Initialise attributes when class is called/initialise
         self.id = user_id
+        self.username = username
+        # Default attributes
+        self.followers = 0
+        self.following = 0
+
+    # self calls from its class(User) Below code is a method
+    def follow(self, user):
+        user.followers += 1
+        self.following += 1
 
 
 # Initialise object, when attributes are added into the class, we can pass on the respective arguments
@@ -13,4 +22,8 @@ user_1 = User("001", "Cesar")
 # user_1.username = "Cesar"
 user_2 = User("002", "Joan")
 
-print(user_1.id)
+user_1.follow(user_2)
+print(user_1.followers)
+print(user_1.following)
+print(user_2.followers)
+print(user_2.following)
